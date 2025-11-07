56b72e19a56bc4c2fdadb9c03e4eecc923d61318
David Castaneda <davidc12997@gmail.com>
2025-11-06 22:08:35 -0500

    I added a in backend a db layer to connect to mysql using config.ts , i also added syste.repository and system.services i update all repositoriesto first try to get data directly from mysql if not possible try to use the cms api, i frontend i create de section contact component and the map component,the customhook for maps is misisng we need to finish it

--- frontend/components/map/useGoogleMap.ts
+++ frontend/components/map/useGoogleMap.ts
@@ -4,7 +4,7 @@ import { setOptions, importLibrary } from "@googlemaps/js-api-loader";
 import { useEffect, useRef, useState } from "react";
 
 type useGoogleMapProps = {
-    htmlRef: React.RefObject<HTMLDivElement>;
+    
     center?: { lat: number; lng: number };
     zoom?: number;
     markers?: Array<{
@@ -13,8 +13,16 @@ type useGoogleMapProps = {
     }>;
 };
 
-const useGoogleMap = ({htmlRef, center, zoom, markers}: useGoogleMapProps) => {
+const useGoogleMap = () => {
  
+    const htmlRef = useRef<HTMLDivElement | null>(null);
+    const [map, setMap] = useState<google.maps.Map | null>(null);
+
+    return{
+        htmlRef,
+        map,
+
+    }
 };
 
 export default useGoogleMap;
